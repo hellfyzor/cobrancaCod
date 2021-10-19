f05a404d43c8be1ea521302b473c13e3f168775c
Márcio Andrade <marcioada@gmail.com>
2021-10-19 14:48:13 -0300

    Primeira parte.

---
+++ src/main/java/com/marcio/cobranca/CobrancaApplication.java
@@ -0,0 +1,13 @@
+package com.marcio.cobranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobrancaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CobrancaApplication.class, args);
+    }
+
+}
